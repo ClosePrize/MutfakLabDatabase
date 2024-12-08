1757d445551d9295572d57aeb97fca1026ebab3c
ClosePrize <circimustafaa@gmail.com>
2024-12-09 01:51:37 +0300

    yemek sitesi için ilk veri çekimi hazırlandı

--- create_database.py
+++ create_database.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import selenium
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -51,7 +52,7 @@ def scroll_and_wait(driver, xpath, timeout=10):
     print(f"Element bulundu: {xpath}")
     
 
-def NefisYemekTarifleri(driver,desiredRecipeNumber):
+def NefisYemekTarifleri(driver, ingreditiendsList, desiredRecipeNumber):
     newUrl = changeUrl(websiteUrl=url[0], ingredients="Domates", queryLinkNumber=0)
     driver.get(newUrl)
     iteration = desiredRecipeNumber // 12
@@ -104,19 +105,109 @@ def NefisYemekTarifleri(driver,desiredRecipeNumber):
     # print(asd333.text)
     # print(asda3.get_attribute("src"))
 
-def Yemek(driver, desiredRecipeNumber):
+def Yemek(driver, ingreditiendsList, desiredRecipeNumber):
     # 9 21 45 57 81 105 129 141 177 189 213 237
-    newUrl = changeUrl(websiteUrl=url[1], ingredients="Domates", queryLinkNumber=1)
-    driver.get(newUrl)
-    iteration = desiredRecipeNumber // 24
-    if iteration > 0:
-        WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div/div[3]/main/section[2]/div[3]")))
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        for i in range(0,iteration):
-            time.sleep(2)
+    for i in range(0,len(ingreditiendsList)):
+        newUrl = changeUrl(websiteUrl=url[1], ingredients=ingreditiendsList[i], queryLinkNumber=1)
+        driver.get(newUrl)
+        iteration = desiredRecipeNumber // 24
+        if iteration > 0:
+            WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,"/html/body/div/div/div[3]/main/section[2]/div[3]")))
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    # recipes = driver.find_element(by=By.CLASS_NAME, value="py-0").text.split("\n")
-    recipes = driver.find_element(by=By.XPATH, value="/html/body/div/div/div[3]/main/section[2]/div[3]").text.split("\n")
-    print(recipes)
-Yemek(driver, 105)
-# NefisYemekTarifleri(driver,25)
+            for i in range(0,iteration):
+                time.sleep(5)
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # recipes = driver.find_element(by=By.CLASS_NAME, value="py-0").text.split("\n")
+        recipes = driver.find_element(by=By.XPATH, value="/html/body/div/div/div[3]/main/section[2]/div[3]").text.split("\n")
+        for i in range(0,3):
+            recipeName = recipes[i*5+3]
+            # WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,f"/html/body/div/div/div[3]/main/section[2]/div[3]/div[{i}]")))
+            recipeLink = driver.find_element(by=By.XPATH, value=f"/html/body/div/div/div[3]/main/section[2]/div[3]/div[{i+1}]/div/a").get_attribute("href")
+            driver.switch_to.parent_frame()
+            time.sleep(0.5)
+            try:
+                # //*[@id="__next"]/div/div[3]/main/section[2]/div[3]/div[1]/div/a/div/div[1]/div/span/img
+                recipeImageLink = driver.find_element(by=By.XPATH, value=f"//*[@id='__next']/div/div[3]/main/section[2]/div[3]/div[{i+1}]/div/a/div/div[1]/div/span/img").get_attribute("src")
+            except selenium.common.exceptions.NoSuchElementException:
+                print(f"Öğe bulunamadı: {recipeName}")
+                recipeImageLink =  ''
+                pass
+            # recipeImageLink = driver.find_element(by=By.XPATH, value=f"/html/body/div/div/div[3]/main/section[2]/div[3]/div[{i+1}]/div/a/div/div[1]/div/span/img").get_attribute("src")
+            # recipeImageLink = WebDriverWait(driver, 10).until(EC.element_located_to_be_selected((By.XPATH, f"/html/body/div/div/div[3]/main/section[2]/div[3]/div[{i+1}]/div/a/div/div[1]/div/span/img"))).get_attribute("src")        
+            driver.switch_to.parent_frame()
+            driver.execute_script("window.open('');")  # Yeni sekme aç
+            driver.switch_to.window(driver.window_handles[1])  # Yeni sekmeye geçiş yap
+            driver.get(recipeLink)
+            recipeIngredients = driver.find_element(by=By.CLASS_NAME, value = "Ingredients_ingredientList__DhBO1").text.split("\n")
+            numberOfIngreditiens = len(recipeIngredients) // 3
+            ingreditiends = []
+            ingreditiendsDetails = []
+            for j in range(0,numberOfIngreditiens):
+                ingreditiends.append(recipeIngredients[j*3+2])
+                ingreditiendsDetails.append(recipeIngredients[j*3]+ ' ' + recipeIngredients[j*3+1])
+            print(recipeName)
+            print(recipeLink)
+            print(recipeImageLink)
+            print(ingreditiends)
+            print(ingreditiendsDetails)
+            print("*******************************************")
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
+            # /html/body/div/div/div[3]/main/section[2]/div/div[1]/section/div[3]/div/div[2]/ul/li[1]
+            # /html/body/div/div/div[3]/main/section[2]/div/div[1]/section/div[3]/div/div[2]/ul/li[1]
+            # /html/body/div/div/div[3]/main/section[2]/div/div[1]/section/div[3]/div/div[2]/ul/li[1]
+            # /html/body/div/div/div[3]/main/section[2]/div[3]/div[1]
+            # /html/body/div/div/div[3]/main/section[2]/div[3]/div[2]
+            # /html/body/div/div/div[3]/main/section[2]/div[3]/div[1]/div/a // recipeLink
+            # /html/body/div/div/div[3]/main/section[2]/div[3]/div[1]/div/a/div/div[1]/div/span/img // recipeImageLink
+        recipes = driver.find_element(by=By.XPATH, value="/html/body/div/div/div[3]/main/section[2]/div[4]").text.split("\n")
+        numberOfRecipes = len(recipes) // 5
+        for i in range(0,numberOfRecipes):
+            # recipeName = recipes[i*5+3]
+            recipeName = driver.find_element(by=By.XPATH, value=f"/html/body/div/div/div[3]/main/section[2]/div[4]/div/div[1]/div[{i+1}]/div/div/h4/a").text
+            driver.switch_to.parent_frame()
+            # /html/body/div/div/div[3]/main/section[2]/div[4]/div/div[1]/div[1]
+            # /html/body/div/div/div[3]/main/section[2]/div[4]/div/div[1]/div[1]/div/a
+            # /html/body/div/div/div[3]/main/section[2]/div[4]/div/div[1]/div[1]/div/a
+            # /html/body/div/div/div[3]/main/section[2]/div[4]/div/div[1]/div[1]
+            # /html/body/div/div/div[3]/main/section[2]/div[4]/div/div[1]/div[2]
+            # /html/body/div/div/div[3]/main/section[2]/div[4]/div/div[1]/div[1]/div/a
+            recipeLink = driver.find_element(by=By.XPATH, value=f"/html/body/div/div/div[3]/main/section[2]/div[4]/div/div[1]/div[{i+1}]/div/a").get_attribute("href")
+            driver.switch_to.parent_frame()
+            # /html/body/div/div/div[3]/main/section[2]/div[4]/div/div[1]/div[1]/div/a/div/div[1]/div/span/img
+            time.sleep(0.5)
+            try:
+                # //*[@id="__next"]/div/div[3]/main/section[2]/div[4]/div/div[1]/div[1]/div/a/div/div[1]/div/span/img
+                # //*[@id="__next"]/div/div[3]/main/section[2]/div[4]/div/div[1]/div[2]/div/a/div/div[1]/div/span/img
+                recipeImageLink = driver.find_element(by=By.XPATH, value=f"//*[@id='__next']/div/div[3]/main/section[2]/div[4]/div/div[1]/div[{i+1}]/div/a/div/div[1]/div/span/img").get_attribute("src")
+            except selenium.common.exceptions.NoSuchElementException:
+                print(f"Öğe bulunamadı: {recipeName}")
+                recipeImageLink =  ''
+                pass
+            # recipeImageLink = driver.find_element(by=By.XPATH, value=f"/html/body/div/div/div[3]/main/section[2]/div[4]/div/div[{i+1}]/div/a/div/div[1]/div/span/img").get_attribute("src")
+            driver.switch_to.parent_frame()
+            driver.execute_script("window.open('');")  # Yeni sekme aç
+            driver.switch_to.window(driver.window_handles[1])  # Yeni sekmeye geçiş yap
+            driver.get(recipeLink)
+            recipeIngredients = driver.find_element(by=By.CLASS_NAME, value = "Ingredients_ingredientList__DhBO1").text.split("\n")
+            numberOfIngreditiens = len(recipeIngredients) // 3
+            ingreditiends = []
+            ingreditiendsDetails = []
+            for j in range(0,numberOfIngreditiens):
+                ingreditiends.append(recipeIngredients[j*3+2])
+                ingreditiendsDetails.append(recipeIngredients[j*3]+ ' ' + recipeIngredients[j*3+1])
+            print(recipeName)
+            print(recipeLink)
+            print(recipeImageLink)
+            print(ingreditiends)
+            print(ingreditiendsDetails)
+            print("*******************************************")
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
+        # print(recipes)
+
+if __name__ == "__main__":
+    ingreditiendsList = ['Domates','Yumurta','Un','Pirinç','Makarna','Tavuk','Kıyma','Patates','Soğan','Sarımsak']
+    ingreditiendsListTest = ['Domates']
+    Yemek(driver, ingreditiendsListTest, 105)
+    # NefisYemekTarifleri(driver, ingreditiendsList, 25)
